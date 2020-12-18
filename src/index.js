ad0c469d03560ec3f66313a9b895286771f25a98
mishikaraj <mishikaraj99@gmail.com>
2020-12-18 21:17:47 +0530

    Greeting based on Time

--- src/index.js
+++ src/index.js
@@ -10,29 +10,55 @@ const currdate=new Date().toLocaleDateString();
 const img1="https://picsum.photos/200/300";
 const img2="https://picsum.photos/250/300";
 const img3="https://picsum.photos/300/300";
+let greeting='';
 
+
+const cssStyle={ 
+}
+
+
+let currDate=new Date(2020,12,6 ,14);
+currDate=currDate.getHours();
+
+if(currDate>=1 && currDate<12)
+{
+  greeting="Good Morning";
+  cssStyle.color="green";
+}
+else if(currDate>12&& currDate<=19)
+{
+  greeting="Good AfterNoon";
+  cssStyle.color="Orange";
+}
+else{
+  greeting="Good Night";
+  cssStyle.color="Black";
+}
 
 
 ReactDOM.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  [
- <h1 className="heading">Hello World!!</h1>,
- <h2>{`my name is ${fname} ${lname}`}</h2>,
+//   [
+//  <h1 className="heading">Hello World!!</h1>,
+//  <h2>{`my name is ${fname} ${lname}`}</h2>,
  
-<p>curr date is ={currdate}</p>,
+// <p>curr date is ={currdate}</p>,
 
- <div className="img_div">
+//  <div className="img_div">
 
 
 
-<img src={img1}/>,
+// <img src={img1}/>,
 
-<img src={img2}/>,
-<img src={img3}/>,
-</div>,
-  ],
+// <img src={img2}/>,
+// <img src={img3}/>,
+// </div>,
+//   ],
+
+
+  <h1> hello Mam , <span style={cssStyle}>{greeting}</span></h1>,  
   
   document.getElementById('root')
 );
